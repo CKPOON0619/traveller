15bc16a24a486814157967b15931b6e3aa0c640f
CKPOON0619 <ckpoon19890129@gmail.com>
2022-03-19 23:18:46 +0000

    CityCard tests and index

--- packages/client/src/shared/CityCard/CityCard.test.tsx
+++ packages/client/src/shared/CityCard/CityCard.test.tsx
@@ -3,14 +3,12 @@ import { BrowserRouter } from 'react-router-dom'
 import { screen, render, fireEvent } from '@testing-library/react'
 import { CityCard } from './CityCard'
 import type { ApolloError } from '@apollo/client'
-// { id: 1, name: 'Test City', country: 'Test country', visited: true, wishlist: true }
 
 describe('<CityCard /> component', () => {
   const testId = 'CityCard'
   const wishText = 'I Wish!'
   const visitText = 'Visited'
   const cityFacts = { id: 1, name: 'Test city', country: 'Test country' }
-  const cityUserConfig = { visited: true, wishlist: true }
 
   it('renders the correct loading placeholder', async () => {
     render(<CityCard cityInfo={undefined} error={undefined} loading={true} onCityChange={() => {}} />)
@@ -31,37 +29,103 @@ describe('<CityCard /> component', () => {
     render(<CityCard cityInfo={cityFacts} error={undefined} loading={false} onCityChange={() => {}} />)
     expect(await screen.findByText(cityFacts.name)).toBeVisible()
     expect(await screen.findByText(cityFacts.country)).toBeVisible()
-    expect(await screen.findByTestId(testId)).not.toContain(wishText)
-    expect(await screen.findByTestId(testId)).not.toContain(visitText)
+    const card = await screen.findByTestId(testId)
+    expect(card).not.toContain(wishText)
+    expect(card).not.toContain(visitText)
   })
-  it('renders the Card with optional CityUserConfig when given', async () => {
-    render(
-      <CityCard
-        cityInfo={{ ...cityFacts, ...cityUserConfig }}
-        error={undefined}
-        loading={false}
-        onCityChange={() => {}}
-      />
-    )
-    expect(await screen.findByText(wishText)).toBeVisible()
-    expect(await screen.findByText(visitText)).toBeVisible()
+  describe('renders the Card with optional CityUserConfig when given', () => {
+    it('renders the card with wishlist when provided', async () => {
+      render(
+        <CityCard
+          cityInfo={{ ...cityFacts, wishlist: true }}
+          error={undefined}
+          loading={false}
+          onCityChange={() => {}}
+        />
+      )
+      const card = await screen.findByTestId(testId)
+      expect(card).not.toHaveTextContent(visitText)
+      expect(card).toHaveTextContent(wishText)
+    })
+    it('renders the card with visited when provided', async () => {
+      render(
+        <CityCard
+          cityInfo={{ ...cityFacts, visited: true }}
+          error={undefined}
+          loading={false}
+          onCityChange={() => {}}
+        />
+      )
+      const card = await screen.findByTestId(testId)
+      expect(card).toHaveTextContent(visitText)
+      expect(card).not.toHaveTextContent(wishText)
+    })
   })
-  it('calls mutation handler with correct data', async () => {
-    const cityUserConfig = { visited: true, wishlist: true }
-    const jestCityChange = jest.fn()
-    render(
-      <CityCard
-        cityInfo={{ ...cityFacts, ...cityUserConfig }}
-        error={undefined}
-        loading={false}
-        onCityChange={jestCityChange}
-      />
-    )
-    const wishButton = await screen.findByText(wishText)
-    const visitButton = await screen.findByText(visitText)
-    fireEvent.click(wishButton)
-    expect(jestCityChange).toBeCalledWith({ id: 1, wishlist: false })
-    fireEvent.click(visitButton)
-    expect(jestCityChange).toBeCalledWith({ id: 1, visited: false })
+  describe('calls mutation handler with correct data', () => {
+    describe('calls mutation handler correctly with visited click', () => {
+      it('should remove the city from visisted list when it is already in the visited list', async () => {
+        const cityUserConfig = { visited: true }
+        const jestCityChange = jest.fn()
+        render(
+          <CityCard
+            cityInfo={{ ...cityFacts, ...cityUserConfig }}
+            error={undefined}
+            loading={false}
+            onCityChange={jestCityChange}
+          />
+        )
+        const visitButton = await screen.findByText(visitText)
+        fireEvent.click(visitButton)
+        expect(jestCityChange).toBeCalledWith({ id: 1, visited: false })
+      })
+      it('should add the city to visisted list when it is not in the visited list', async () => {
+        const cityUserConfig = { visited: false }
+        const jestCityChange = jest.fn()
+        render(
+          <CityCard
+            cityInfo={{ ...cityFacts, ...cityUserConfig }}
+            error={undefined}
+            loading={false}
+            onCityChange={jestCityChange}
+          />
+        )
+        const visitButton = await screen.findByText(visitText)
+        fireEvent.click(visitButton)
+        expect(jestCityChange).toBeCalledWith({ id: 1, visited: true })
+      })
+    })
+
+    describe('calls mutation handler correctly with wishlist click', () => {
+      it('should remove city from wishllist is it is already in wishlist', async () => {
+        const cityUserConfig = { wishlist: true }
+        const jestCityChange = jest.fn()
+        render(
+          <CityCard
+            cityInfo={{ ...cityFacts, ...cityUserConfig }}
+            error={undefined}
+            loading={false}
+            onCityChange={jestCityChange}
+          />
+        )
+        const wishButton = await screen.findByText(wishText)
+        fireEvent.click(wishButton)
+        expect(jestCityChange).toBeCalledWith({ id: 1, wishlist: false })
+      })
+      it('should add city to wishllist is it is not in wishlist', async () => {
+        const cityUserConfig = { wishlist: false }
+        const jestCityChange = jest.fn()
+        render(
+          <CityCard
+            cityInfo={{ ...cityFacts, ...cityUserConfig }}
+            error={undefined}
+            loading={false}
+            onCityChange={jestCityChange}
+          />
+        )
+        const wishButton = await screen.findByText(wishText)
+        fireEvent.click(wishButton)
+        expect(jestCityChange).toBeCalledWith({ id: 1, wishlist: true })
+      })
+    })
   })
 })
