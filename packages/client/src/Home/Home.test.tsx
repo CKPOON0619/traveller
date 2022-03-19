eb63eb5df833dee9f2064ea5a53db0cd2ba356e5
CKPOON0619 <ckpoon19890129@gmail.com>
2022-03-19 22:48:27 +0000

    Add home page

--- packages/client/src/Home/Home.test.tsx
+++ packages/client/src/Home/Home.test.tsx
@@ -1,3 +1,33 @@
-import * as React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { SearchResult } from './SearchResult'
+import type { ApolloError } from '@apollo/client'
+import { Home } from './Home'
 
-//TODO: test for search behavior
+jest.mock('./SearchResult', () => ({
+  SearchResult: ({ searchName }: { searchName: string }) => {
+    return <div>{`<search result for ${searchName}>`}</div>
+  },
+}))
+
+describe('<Home /> component', () => {
+  it('should show a search box', async () => {
+    render(<Home />)
+    expect(await screen.findByTestId('search-input')).toBeVisible()
+  })
+  it('Upon confirm it should pass the search value to search result component for searching', async () => {
+    render(<Home />)
+    const inputField = await screen.findByTestId('search-input')
+    const searchIcon = await screen.findByTestId('search-icon')
+    fireEvent.focus(inputField)
+    fireEvent.change(inputField, { target: { value: 'London' } })
+    expect(await screen.findByDisplayValue('London')).toBeVisible()
+    fireEvent.click(searchIcon)
+    expect(await screen.findByText('<search result for London>')).toBeVisible()
+    fireEvent.focus(inputField)
+    fireEvent.change(inputField, { target: { value: 'Paris' } })
+    expect(await screen.findByDisplayValue('Paris')).toBeVisible()
+    fireEvent.focus(inputField)
+    fireEvent.keyDown(inputField, { key: 'Enter' })
+    expect(await screen.findByText('<search result for Paris>')).toBeVisible()
+  })
+})
--- packages/client/src/Home/Home.tsx
+++ packages/client/src/Home/Home.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import { Container, InputRightElement, Input, Heading, InputGroup, IconButton, VStack } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
-import { useQuery } from '@apollo/client'
+import { SearchResult } from './SearchResult'
 
 export const Home: React.VoidFunctionComponent = () => {
-  return <VStack spacing="8">
-    <Heading as="h1">Smart traveller</Heading>
-    <Container maxW="container.md">
-      <InputGroup>
-        <Input />
-        <InputRightElement children={<IconButton aria-label="" icon={<Search2Icon />} />} />
-      </InputGroup>
-    </Container>
-  </VStack>
+  const searchRef = React.useRef<HTMLInputElement>(null)
+  const [confirmedSearch, setconfirmedSearch] = React.useState<string | undefined>(undefined)
+
+  const handleClickConfirmedSearch = React.useCallback(() => {
+    setconfirmedSearch(searchRef.current?.value)
+  }, [])
+  const handleEnterConfirmedSearch = React.useCallback(e => {
+    //TODO: check compatibility across browser and devices
+    if (e.key === 'Enter') {
+      setconfirmedSearch(searchRef.current?.value)
+    }
+  }, [])
+
+  return (
+    <VStack spacing="8">
+      <Heading as="h1">Smart traveller</Heading>
+      <Container maxW="container.md">
+        <InputGroup>
+          <Input data-testid="search-input" ref={searchRef} onKeyDown={handleEnterConfirmedSearch} />
+          <InputRightElement
+            children={
+              <IconButton
+                aria-label=""
+                icon={<Search2Icon data-testid="search-icon" onClick={handleClickConfirmedSearch} />}
+              />
+            }
+          />
+        </InputGroup>
+        {confirmedSearch ? <SearchResult searchName={confirmedSearch} /> : null}
+      </Container>
+    </VStack>
+  )
 }
