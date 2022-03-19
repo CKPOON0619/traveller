fa228c4597e0d67070e3e7d97dd756c47c1d116a
CKPOON0619 <ckpoon19890129@gmail.com>
2022-03-19 20:26:02 +0000

    CityCard and tests

--- packages/client/src/shared/CityCard/CityCard.test.tsx
+++ packages/client/src/shared/CityCard/CityCard.test.tsx
@@ -1,11 +1,67 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import { screen } from '@testing-library/react'
+import { screen, render, fireEvent } from '@testing-library/react'
+import { CityCard } from './CityCard'
+import type { ApolloError } from '@apollo/client'
+// { id: 1, name: 'Test City', country: 'Test country', visited: true, wishlist: true }
 
 describe('<CityCard /> component', () => {
-  it('renders the correct loading placeholder', () => {})
-  it('renders the correct error placeholder', () => {})
-  it('renders the Card with CityFacts', () => {})
-  it('renders the Card with optional CityUserConfig', () => {})
-  it('calls mutation handler with correct data', () => {})
+  const testId = 'CityCard'
+  const wishText = 'I Wish!'
+  const visitText = 'Visited'
+  const cityFacts = { id: 1, name: 'Test city', country: 'Test country' }
+  const cityUserConfig = { visited: true, wishlist: true }
+
+  it('renders the correct loading placeholder', async () => {
+    render(<CityCard cityInfo={undefined} error={undefined} loading={true} onCityChange={() => {}} />)
+    expect(await screen.findByText('Loading...')).toBeVisible()
+  })
+  it('renders the correct error placeholder', async () => {
+    render(
+      <CityCard
+        cityInfo={undefined}
+        error={{ message: 'some error' } as ApolloError}
+        loading={false}
+        onCityChange={() => {}}
+      />
+    )
+    expect(await screen.findByText('Error :(')).toBeVisible()
+  })
+  it('renders the Card with only CityFacts when no CityUserConfig given', async () => {
+    render(<CityCard cityInfo={cityFacts} error={undefined} loading={false} onCityChange={() => {}} />)
+    expect(await screen.findByText(cityFacts.name)).toBeVisible()
+    expect(await screen.findByText(cityFacts.country)).toBeVisible()
+    expect(await screen.findByTestId(testId)).not.toContain(wishText)
+    expect(await screen.findByTestId(testId)).not.toContain(visitText)
+  })
+  it('renders the Card with optional CityUserConfig when given', async () => {
+    render(
+      <CityCard
+        cityInfo={{ ...cityFacts, ...cityUserConfig }}
+        error={undefined}
+        loading={false}
+        onCityChange={() => {}}
+      />
+    )
+    expect(await screen.findByText(wishText)).toBeVisible()
+    expect(await screen.findByText(visitText)).toBeVisible()
+  })
+  it('calls mutation handler with correct data', async () => {
+    const cityUserConfig = { visited: true, wishlist: true }
+    const jestCityChange = jest.fn()
+    render(
+      <CityCard
+        cityInfo={{ ...cityFacts, ...cityUserConfig }}
+        error={undefined}
+        loading={false}
+        onCityChange={jestCityChange}
+      />
+    )
+    const wishButton = await screen.findByText(wishText)
+    const visitButton = await screen.findByText(visitText)
+    fireEvent.click(wishButton)
+    expect(jestCityChange).toBeCalledWith({ id: 1, wishlist: false })
+    fireEvent.click(visitButton)
+    expect(jestCityChange).toBeCalledWith({ id: 1, visited: false })
+  })
 })
---
+++ packages/client/src/shared/CityCard/CityCard.tsx
@@ -0,0 +1,81 @@
+import type { ApolloError } from '@apollo/client'
+import { Box, Image, Flex, Text } from '@chakra-ui/react'
+import { MdStar, MdStarBorder, MdAddLocation, MdLocationOn } from 'react-icons/md'
+
+import * as React from 'react'
+import type { CityID, CityFacts, CityUserConfig, CityMutation } from '../dataHooks/type'
+
+interface CityCardProps {
+  cityInfo?: CityID & CityFacts & Partial<CityUserConfig>
+  error?: ApolloError
+  loading?: boolean
+  onCityChange?: (changes: CityMutation) => void
+}
+
+export const CityCard: React.VoidFunctionComponent<CityCardProps> = ({ cityInfo, error, loading, onCityChange }) => {
+  const handleWishlistChange = React.useCallback(() => {
+    if (onCityChange && cityInfo?.wishlist !== undefined) {
+      onCityChange({ id: cityInfo.id, wishlist: !cityInfo?.wishlist })
+    }
+  }, [onCityChange, cityInfo?.wishlist])
+  const handleVisitChange = React.useCallback(() => {
+    if (onCityChange && cityInfo?.visited !== undefined) {
+      onCityChange({ id: cityInfo.id, visited: !cityInfo?.visited })
+    }
+  }, [onCityChange, cityInfo?.visited])
+
+  if (cityInfo === undefined && loading) {
+    return (
+      <Box p="5" maxW="320px" borderWidth="1px">
+        <Flex mt={2} align="center">
+          <Text ml={1} fontSize="sm">
+            <b>Loading...</b>
+          </Text>
+        </Flex>
+      </Box>
+    )
+  }
+  if (cityInfo === undefined || error) {
+    return (
+      <Box p="5" maxW="320px" borderWidth="1px">
+        <Flex mt={2} align="center">
+          <Text ml={1} fontSize="sm">
+            <b>Error :(</b>
+          </Text>
+        </Flex>
+      </Box>
+    )
+  }
+
+  const { name, country, wishlist, visited } = cityInfo
+
+  return (
+    <Box data-testid="CityCard" p="5" maxW="320px" borderWidth="1px">
+      <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
+      <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+        {country}
+      </Text>
+      <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+        {name}
+      </Text>
+      {wishlist !== undefined ? (
+        <Flex cursor={onCityChange ? 'pointer' : 'default'} onClick={handleWishlistChange} mt={2} align="center">
+          <Box as={wishlist ? MdStar : MdStarBorder} color="orange.400" />
+          <Text ml={1} fontSize="sm">
+            <b>I Wish!</b>
+          </Text>
+        </Flex>
+      ) : null}
+      {visited !== undefined ? (
+        <Flex cursor={onCityChange ? 'pointer' : 'default'} onClick={handleVisitChange} mt={2} align="center">
+          <Box as={visited ? MdLocationOn : MdAddLocation} color={visited ? 'orange.400' : 'grey.400'} />
+          <Text ml={1} fontSize="sm">
+            <b>Visited</b>
+          </Text>
+        </Flex>
+      ) : null}
+    </Box>
+  )
+}
+
+export const MemoizedCityCard = React.memo(CityCard)
